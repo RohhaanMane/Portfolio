5fc4bcd8de42b7779a1710a11f9727cd00ae51e2
RohhaanMane <rohanmane2929r@gmail.com>
2023-03-01 16:20:50 +0530

    missing index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+
+import Projects from './components/Projects/Projects';
 import './App.css'
 
 function App() {
-  return "Hello world";
+  return (
+    <Projects />
+  )
 }
 
 export default App
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+
+const Projects = () =>{
+    return (
+        <Fragment>
+            <h2>Projects</h2>
+            <div>List of cards</div>
+        </Fragment>
+        
+    );
+
+}
+
+export default Projects;
