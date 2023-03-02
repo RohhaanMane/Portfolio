bb6f59f9444aeaa3f186dfc488de0803ebc80cfc
RohhaanMane <rohanmane2929r@gmail.com>
2023-03-02 08:49:13 +0530

    Designed Project card

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,14 +1,66 @@
 import React, { Fragment } from "react";
+import "./Projects.css";
 
-const Projects = () =>{
-    return (
-        <Fragment>
-            <h2>Projects</h2>
-            <div>List of cards</div>
-        </Fragment>
-        
-    );
+const Projects = () => {
+  const projects = [
+    {
+      name: "Project 1",
+      description:
+        "Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam",
+      stack: ["SASS", "TypeScript", "React"],
+      sourceCode: "https://github.com",
+      livePreview: "https://github.com",
+    },
+    {
+      name: "Project 2",
+      description:
+        "Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam",
+      stack: ["SASS", "TypeScript", "React"],
+      sourceCode: "https://github.com",
+      livePreview: "https://github.com",
+    },
+    {
+      name: "Project 3",
+      description:
+        "Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam",
+      stack: ["SASS", "TypeScript", "React"],
+      sourceCode: "https://github.com",
+      livePreview: "https://github.com",
+    },
+  ];
+  return (
+    <Fragment>
+      <h2>Projects</h2>
+      <div className="project__grid">
+        <div className="project">
+          <h3>{projects[0].name}</h3>
+          <p className="project__description">{projects[0].description}</p>
 
-}
+          {/* {<ul className="project-stack">
+                        {projects[0].stack.map(item=>{
+                            <li className="project-stack__item">{item}</li>
+                        })}
+                        </ul>} */}
+
+          {/* {
+                        <ul>
+                           {projects[0].stack.map(item=>{
+                            <li>{item}</li>
+                           })} 
+                        </ul>
+                    } */}
+
+          <ul className="project-stack">
+            {projects[0].stack.map((item) => (
+              <li  className="project-stack__item">
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 
 export default Projects;
