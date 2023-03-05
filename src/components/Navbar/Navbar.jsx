bd5832ddb5f88425336b29d8b5dc3a0d0dbb0a02
Minesh Rajput <110811001+Minesh78@users.noreply.github.com>
2023-03-05 10:59:43 +0530

    added logo ani nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,25 @@
 import React from "react";
-
+import logo from "./assets/minesh-logo.svg"
 const Navbar =()=>{
 
+    // const navLinks = ["home", "skills","projects", "contact"]
+
+    // const Nav = navLinks.map(item =>  {return <li> {item} </li>})
+
     return(
-        <h1 className="text-red-600 font-['Poppins']">Projects</h1>
+    <>
+        <nav className="flex-auto flex-row gap-1 m-2 ">
+            <div>
+                <img className="w-10" alt="logo" src={logo}/>
+            </div>
+            <ul className="">      
+                <li>home</li>
+                <li>skills</li>
+                <li>projects</li>
+                <li>contact</li>
+            </ul>
+        </nav>
+    </>
     )
 }
 
