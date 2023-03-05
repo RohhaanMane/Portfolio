eefa204e46cba97facde5de3e9ad87eda86f01d0
Minesh Rajput <110811001+Minesh78@users.noreply.github.com>
2023-03-05 14:48:19 +0530

    added nav bar working on responsiveness of it

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <div>
     <Navbar />
 
-    <Projects />
+    {/* <Projects /> */}
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,5 +7,16 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+
+  daisyui: {
+    styled: false,
+    themes: false,
+    base: false,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "li",
+  },
 }
