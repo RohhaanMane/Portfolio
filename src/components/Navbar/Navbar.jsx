66108eb8acc63e35f91391a3304af6147ef373e0
Minesh Rajput <110811001+Minesh78@users.noreply.github.com>
2023-03-05 19:24:06 +0530

    added responsive nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React ,{useState}from "react";
 import logo from "./assets/minesh-logo.svg"
 const Navbar =()=>{
 
@@ -9,23 +9,26 @@ const Navbar =()=>{
         {name:"contact",link:"/"},
     ]
 
-    const navItems = navLinks.map(item => <li key={item.link} className=" text-xl md:px-3  md:my-0 my-7"> <a href={item.link} className= "font-medium hover:text-gray-500 ease-in-out">{item.name}</a></li>)
+    const navItems = navLinks.map(item => <li key={item.link} className=" text-xl md:px-3  md:my-0 my-7 border-2 border-rose-500 "> <a href={item.link} className= "hover:text-gray-500 ">{item.name}</a></li>)
 
-    
+    const [open, setOpen] = useState(false);
 
     return(
     <>
-        <nav className="w-full fixed  ">
-            <div className=" max-w-6xl mx-auto md:flex md:items-center md:justify-between py-5" >
+        <nav className="w-full fixed  shadow-sm ">
+            <div className=" max-w-6xl mx-auto md:flex md:items-center md:justify-between py-5 bg-white" >
                 <div className="font-bold items-center text-2xl cursor-pointer flex ">
-                    <img  src={logo} alt="logo" className="h-10 mx-5"></img> 
+                    <img  src={logo} alt="logo" className="h-10 mx-5 "></img> 
                 </div>
-                <div className="md:flex items-center justify-center uppercase  ">
-                    <ul className="md:flex justify-items md:pb-0 pb-12">
+                <div onClick={()=>setOpen(!open)} className="text-3xl  absolute right-8 top-5 cursor-pointer md:hidden">
+                <ion-icon  className="transition-all duration-500 ease-in-out" name={open? "close" : "menu"}></ion-icon>
+                </div>
+                <div className={`md:flex items-center uppercase md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] md:right-0 w-full md:w-auto md:pl-0 pl-5 transition-all  backdrop-blur-sm duration-500  ease-in-out ${open? "top-20 opacity-100":"top-[-490px] md:opacity-100 opacity-0"} `}>
+                    <ul className="md:flex md:items-center ">
                         {navItems}
                     </ul>
                     
-                    <label className="swap swap-rotate mx-4 ">
+                    <label className="swap swap-rotate mx-4 md:px-3  md:my-0 my-1 ">
                             <input type="checkbox" />
                         
                             <svg className="swap-on fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
