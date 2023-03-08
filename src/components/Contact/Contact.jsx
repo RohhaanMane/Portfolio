5616aa8b98d19e52f786e9ac3f41b5752e072ac7
RohhaanMane <rohanmane2929r@gmail.com>
2023-03-08 19:36:05 +0530

    Contact details added

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,22 @@
 const Contact = (props) => {
+
+    //Dummy contact mail
+    const contact = {
+        email: 'johnsmith@mail.com',
+      }
+
+      const clickHandler = (e) => {
+        e.preventDefault();
+      }
+
   return (
-    <section id="contact">
-      <h2 className="text-3xl font-bold text-center text-[#444444] mt-20">
+    <section id="contact" className="flex flex-col items-center">
+      <h2 className="text-3xl font-bold text-[#444444] mt-20 mb-8">
         CONTACT
       </h2>
+      <a href="">
+        <button className="border-2 border-[#2978b5] px-[1.26rem] py-[0.72rem] bg-right-bottom hover:bg-[#2978b5] hover:text-white hover:bg-left-bottom hover:transition hover:ease-in-out hover:duration-500" onClick={clickHandler}>Email Me</button>
+      </a>
     </section>
   );
 };
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+const Footer = props =>{
+    return(
+        <h3>Created By </h3>
+    );
+}
+
+export default Footer;
