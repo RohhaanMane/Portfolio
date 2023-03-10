9c97ca10389543744448b6d83d9d4d7471ee65c8
Minesh Rajput <110811001+Minesh78@users.noreply.github.com>
2023-03-10 13:25:59 +0530

    padding finalised

--- src/components/Header/hero.jsx
+++ src/components/Header/hero.jsx
@@ -4,11 +4,11 @@ import React from "react";
 const Hero = () =>{
     return(
         <>
-           <section className="max-w-[80%] m-auto my-10 gap-1" >
-           <div className="flex flex-col items-center">
-            <h2 className="text-2xl md:text-5xl">Hi, I am Minesh Rajput</h2>
-            <h3 className="text-xl my-4 md:text-3xl">Front End Engineer.</h3>
-            <p className="indent-10 justify-content text-lg md:max-w-[60%]">Whether you need a simple website or a complex web application, I can help bring your ideas to life. I have experience working with a variety of projects. My focus is always on delivering a high-quality product that meets your business objectives.</p>
+           <section className="max-w-[80%] m-auto my-[5rem]" >
+           <div className="flex flex-col items-center ">
+            <h2 className="text-3xl font-bold md:text-5xl md:mt-[6rem]">Hi, I am Minesh Rajput</h2>
+            <h3 className="text-xl font-medium mt-3 mb-8 md:text-3xl md:mb-[4rem]">Front End Engineer.</h3>
+            <p className="indent-10 justify-content md:text-lg  md:max-w-[70%]">Whether you need a simple website or a complex web application, I can help bring your ideas to life. I have experience working with a variety of projects. My focus is always on delivering a high-quality product that meets your business objectives.</p>
             </div>
            </section>
         </>
