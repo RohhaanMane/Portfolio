1b2ae9f1992c40ca11fb822a33f005ee6dd783ce
RohhaanMane <rohanmane2929r@gmail.com>
2023-03-11 11:39:05 +0530

    Footer section added & did some enhancement

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import { useState } from 'react'
-import Navbar from './components/Navbar/Navbar';
-import Hero from './components/Header/hero'
+import { useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Header/hero";
 
-import Projects from './components/Projects/Projects';
-import './App.css'
-import Skills from './components/Skills/Skills';
-import Contact from './components/Contact/Contact';
+import Projects from "./components/Projects/Projects";
+import "./App.css";
+import Skills from "./components/Skills/Skills";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div>
-    <Navbar />
-    <Hero />
-    <Projects />
-    <Skills />
-    <Contact />
+      <Navbar />
+      <main className="mx-auto">
+        <Hero />
+        <Projects />
+        <Skills />
+        <Contact />
+      </main>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,14 @@
-const Footer = props =>{
-    return(
-        <h3>Created By </h3>
-    );
-}
+const Footer = (props) => {
+  return (
+    <footer className="px-[3em] my-[4em] text-center">
+       <a
+      href='https://github.com/RohhaanMane/Portfolio'
+      className='link footer__link'
+    >
+      Created By 
+    </a>
+    </footer>
+  );
+};
 
 export default Footer;
